5695f63a10da92ff518f579e63760f274329b480
Drew Schuster <dschuster@nerdwallet.com>
2016-06-02 09:45:17 -0700

    LOL CORS

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var default_assignee = process.env.DEFAULT_ASSIGNEE
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 3000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.status(200).end();
 });
